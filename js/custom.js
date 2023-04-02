9b3072b2d1f2eb58b18e3cedc1cb772c30f8c409
Gautam Dheer <gautam.litesh@gmail.com>
2023-04-02 20:15:13 +0530

    update code

---
+++ js/custom.js
@@ -0,0 +1,110 @@
+$('.slider1').owlCarousel({
+    stagePadding: 30,
+    loop:true,
+    margin:5,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.slider2').owlCarousel({
+    stagePadding: 50,
+    loop:true,
+    margin:5,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.slider3').owlCarousel({
+    stagePadding: 50,
+    loop:true,
+    margin:5,
+    nav:false,
+    dots: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+$('.slider4').owlCarousel({
+    stagePadding: 50,
+    loop:true,
+    margin:5,
+    nav:false,
+    dots: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+$('.slider5').owlCarousel({
+    stagePadding: 50,
+    loop:true,
+    margin:5,
+    nav:false,
+    dots: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+$('.slider6').owlCarousel({
+    stagePadding: 50,
+    loop:true,
+    margin:5,
+    nav:false,
+    dots: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:5
+        }
+    }
+})
